f93daa2fb4c1dbad93c5e9ee046ac1c799fde64f
=jaouad Sourour <jaouad.pxy@gmail.com>
2018-08-30 09:38:41 +1000

    commit changes

--- 02_loops_arrays.rb
+++ 02_loops_arrays.rb
@@ -21,3 +21,61 @@
 # water sell for $6, and cost $0.15 to make
 
 # print out the total profit for the orders you have
+
+# - create a well named variable that contains the amount of time it took you to get to class
+time_to_class_mins = 70
+# - create a complete sentence  that lets us know how you got to class and how long it took
+# - print this complete sentence
+puts "I drove and took the train to class and it took #{time_to_class_mins} minutes."
+
+# you are working at a bar where you have a current backlog of drinks to make:
+# 3 cocktails
+# 2 waters
+# and
+# 6 beers
+drinks = [3, 2, 6]
+
+# write a program that asks the customer for their order.
+# if they order a cocktail, add one to the number of cocktails you need to make,
+# if they order a water, add one to the number of waters you need to make,
+# if they order a beer, add one to the number of beers you need to pour
+drink = ""
+until drink == "quit"
+    puts "What drink do you want to order?"
+    drink = gets.chomp
+    # if drink == "cocktail"
+    #     drinks[0] += 1
+    # elsif drink == "water"
+    #     drinks[1] += 1
+    # elsif drink == "beer"
+    #     drinks[2] += 1
+    # end
+    case drink
+        when "cocktail"
+            drinks[0] += 1
+        when "water"
+            drinks[1] += 1
+        when "beer"
+            drinks[2] += 1
+        else
+            if drink != "quit"
+                puts "We don't have that!"
+            end
+    end
+end
+
+# print the final drinks order so you know what to make
+puts "#{drinks[0]} cocktails, #{drinks[1]} waters, #{drinks[2]} beers."
+
+# ****
+# cocktails sell for $22, and cost $8 to make
+# beer sell for $12, and cost $3 to pour
+# water sell for $6, and cost $0.15 to make
+COCKTAIL_PROFIT = 22 - 8
+BEER_PROFIT = 12 - 3
+WATER_PROFIT = 6 - 0.15
+
+total_profit = (COCKTAIL_PROFIT * drinks[0]) + (BEER_PROFIT * drinks[2]) + (WATER_PROFIT * drinks[1])
+
+# print out the total profit for the orders you have
+puts "The total profit is: #{total_profit}"
--- 04_fib_seq.rb
+++ 04_fib_seq.rb
@@ -1,3 +1,36 @@
 # 1. sit beside someone new, at a new spot :slightly_smiling_face:
 
-# 2. There is an odd employee at The Company who needs some code written that will provide them with the first 100 numbers of the Fibonacci sequence ….. (no one really knows why the odd employee needs this information - or why they can’t work it out themselves - but Monday mornings can be strange).
+# 2. There is an odd employee at The Company who needs some code written that will
+# provide them with the first 100 numbers of the Fibonacci sequence ….. 
+# (no one really knows why the odd employee needs this information - or why they can’t work it out themselves 
+# - but Monday mornings can be strange).
+odd = (1..100) 
+p odd.step(2).to_a
+
+
+=begin
+    
+
+ PS1='\[\033]0;Terminal:$PWD\007\]'      # set window title
+PS1="$PS1"'\n'                          # new line
+PS1="$PS1"'\[\033[32m\]'                # change to brownish yellow
+PS1="$PS1"'JaouadPxy:»\W\n'ls
+
+PS1="$PS1"'\[\033[34m\]'                # change to brownish yellow
+PS1="$PS1"'±» '                         # current working directory
+PS1="$PS1"'\[\033[0m\]'                 # change color
+MSYS2_PS1="$PS1"                        # for detection by MSYS2 SDK's bash.basrc
+How to: cd into your root directory.
+1.> grep PROMPT ~/.bashrc
+2.> grep P ~/.bashrc        # Display .bashrc
+3.> grep pwd ~/.bashrc
+4.> vi ~/.bashrc        # Open file
+5.> :wq                # Window quit
+5.> . ~/.bashrc
+6.> pwd   
+=end
+
+
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
